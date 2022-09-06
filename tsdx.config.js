7660f5ec690812b56e5f4dbe1fe7850faa8f82c5
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2022-09-06 16:39:58 -0300

    fix antd build on tsdx

---
+++ tsdx.config.js
@@ -0,0 +1,22 @@
+const postcss = require('rollup-plugin-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+module.exports = {
+  rollup(config, options) {
+    config.plugins.push(
+      postcss({
+        minimize: true,
+        modules: true,
+        use: {
+          sass: null,
+          stylus: null,
+          less: { javascriptEnabled: true },
+        },
+        extract: true,
+      })
+    );
+
+    return config;
+  },
+};
